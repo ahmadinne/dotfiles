686ce2a5cdcb0d7cb3fbc18e92f9b79dfc3629cb
ahmadinne <fikri.mny@gmail.com>
2025-10-02 18:50:01 +0700

    upd

--- waybar/utilities/util.sh
+++ waybar/utilities/util.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
+barid=$(ps -ef | grep '[w]aybar -c' | grep utilities | awk '{print $2}' )
 
-colorpicker {
-	barid=$(ps -ef | grep '[w]aybar -c' | grep utilities | awk '{print $2}' )
+function colorpicker {
 	kill $barid
 	hyprpicker -a
 }
 
-case $1 in
+function clipboard {
+	kill $barid
+	cliphist list | rofi -dmenu | cliphist decode | wl-copy
+}
+
+choice=$1
+case $choice in
 	colorpicker) colorpicker ;;
+	clipboard) clipboard ;;
+	*) exit ;;
 esac
